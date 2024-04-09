0dc7d6be985461695f0b86ecfc7b8724c9414e51
Damian <dkowalski.1997@gmail.com>
2024-04-09 21:50:36 +0200

    added animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import ArticleTile from "./_components/_tile/article-tile";
+import Transition from "./transition";
 
 export default function MainPage() {
   return (
-    <div className="mt-20 grid gap-5 grid-cols-auto-fit-100">
-      <ArticleTile />
-      <ArticleTile />
-      <ArticleTile />
-      <ArticleTile />
-      <ArticleTile />
-    </div>
+    <Transition>
+      <div className="mt-20 grid gap-5 grid-cols-auto-fit-100">
+        <ArticleTile />
+        <ArticleTile />
+        <ArticleTile />
+        <ArticleTile />
+        <ArticleTile />
+      </div>
+    </Transition>
   );
 }
