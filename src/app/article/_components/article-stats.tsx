a2141f22040063c4887949077c64b3548f92dcdf
Damian <dkowalski.1997@gmail.com>
2024-04-10 02:16:34 +0200

    work in progress

--- src/app/article/_components/article-stats.tsx
+++ src/app/article/_components/article-stats.tsx
@@ -4,18 +4,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function ArticleStats() {  
   return (
-    <div className="flex gap-4 justify-center w-auto py-4">
+    <div className="flex gap-3 justify-center w-auto py-2">
       <div className="group cursor-pointer flex flex-col items-center gap-2 text-xs">
         <div className={`${styles.circle} items-center justify-center transition-all duration-300 flex w-9 h-9 rounded-full cursor-pointer opacity-50 group-hover:opacity-100`}>
-          <FontAwesomeIcon className={styles.icon} icon={faHeart} style={{ maxWidth: '0.8125rem', maxHeight: '0.8125rem' }} color='#000000' />
+          <FontAwesomeIcon className={styles.icon} icon={faHeart} style={{ maxWidth: '0.8125rem', maxHeight: '0.8125rem' }} color='#333333' />
         </div>
-        <span className="font-medium cursor-pointer opacity-50 group-hover:opacity-100">12</span>
+        <span style={{ color: '#535353' }} className="font-medium cursor-pointer opacity-50 group-hover:opacity-100">12</span>
       </div>
       <div className="group cursor-pointer flex flex-col items-center gap-2 text-xs">
         <div className={`${styles.circle} items-center justify-center transition-all duration-300 flex w-9 h-9 rounded-full cursor-pointer opacity-50 group-hover:opacity-100`}>
-          <FontAwesomeIcon className={styles.icon} icon={faComments} style={{ maxWidth: '0.8125rem', maxHeight: '0.8125rem' }} color='#000000' />
+          <FontAwesomeIcon className={styles.icon} icon={faComments} style={{ maxWidth: '0.8125rem', maxHeight: '0.8125rem' }} color='#333333' />
         </div>
-        <span className="font-medium cursor-pointer opacity-50 group-hover:opacity-100">12</span>
+        <span style={{ color: '#535353' }} className="font-medium cursor-pointer opacity-50 group-hover:opacity-100">12</span>
       </div>
     </div>
   )
--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -6,7 +6,7 @@ export default function Article() {
     return (
         <div className="flex flex-col items-center">
             <Transition>
-                <h1 className="max-w-2xl text-4xl py-6 text-center leading-[3rem] font-bold" style={{ color: '#333333' }}>How closures can change the world... or at least your code</h1>
+                <h1 className="max-w-2xl text-4xl py-6 text-center leading-[3rem] font-extrabold" style={{ color: '#333333' }}>How closures can change the world... or at least your code</h1>
             </Transition>
             <Transition delay={0.3}>
                 <AuthorInfo />
