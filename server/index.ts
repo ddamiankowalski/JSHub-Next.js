f253ca5e7f1cc44c2271c401994147c99789acae
Damian <dkowalski.1997@gmail.com>
2024-04-21 22:04:22 +0200

    added typescript on backenmd

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = 4200;
+const dotenv = require('dotenv');
 
 const articleRouter = require('./routes/articles');
-
 const errorHandler = require('./middleware/error-handler');
 
-app.use(bodyParser.json())
+dotenv.config();
 
+const app = express();
+app.use(bodyParser.json());
 app.use('/articles', articleRouter);
 
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
-})
+});
 
 app.use(errorHandler);
