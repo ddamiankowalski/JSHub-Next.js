4db4ebc650748e438c4d3e3d43fdf30e15d41bbc
Damian <dkowalski.1997@gmail.com>
2024-04-22 00:58:56 +0200

    added api prefix in nextjs

--- src/api/articles.ts
+++ src/api/articles.ts
@@ -1,11 +1,11 @@
 const getArticles = async (): Promise<any> => {
-  return fetch('http://localhost:4200/article/all', { cache: 'no-cache' }).then(
-    (res) => res.json()
-  );
+  return fetch('http://localhost:4200/api/article/all', {
+    cache: 'no-cache',
+  }).then((res) => res.json());
 };
 
 const getArticle = async (id: string): Promise<any> => {
-  return fetch('http://localhost:4200/article/' + id, {
+  return fetch('http://localhost:4200/api/article/' + id, {
     cache: 'no-cache',
   }).then((res) => res.json());
 };
